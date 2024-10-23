ac85d95000d692b25ffda8419a05e6ab3792f212
Starkie Phillips <star.phillips03@gmail.com>
2024-10-23 12:12:32 +0100

    add sort function

--- index.py
+++ index.py
@@ -10,6 +10,13 @@ links = soup.select('.titleline > a')  # Get the title and link of the story
 subtext = soup.select('.subtext')  # Get the subtext of each story
 
 
+# New function to sort the articles based on points recieved
+
+def sort_articles(hnlist):
+    # From the dictionary we choose to sort the list by votes
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
 def topstories_hn(links, votes):
     hn = []
     for idx, item in enumerate(links):
@@ -21,7 +28,7 @@ def topstories_hn(links, votes):
             # Only getting stories with points 100 or greater
             if points > 99:
                 hn.append({'story': story, 'link': href, 'votes': points})
-    return hn
+    return sort_articles(hn)
 
 
 pprint.pprint(topstories_hn(links, subtext))
